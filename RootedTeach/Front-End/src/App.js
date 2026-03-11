faa03cbbb82be3e7ed09443d7277107b928b2628
kjusilver <kjusilver0214@gmail.com>
2026-03-11 03:25:04 -0700

    Update App.js

--- RootedTeach/Front-End/src/App.js
+++ RootedTeach/Front-End/src/App.js
@@ -14,6 +14,10 @@ import CourseDashboard from "./StudentView/CourseDashboard";
 import AssignmentDashboard from "./StudentView/AssignmentDashboard";
 import TeacherView from "./TeacherView/Teacher"; 
 import ClassPage from "./Class/ClassPage";
+import AllAssignment from "./StudentView/AllAssignment";
+import Calendar from "./StudentView/Calendar";
+import AllGrades from "./StudentView/AllGrades";
+import Account from "./StudentView/Account";
 
 function App() {
   const [logIn, setLogin] = useState(false);
@@ -32,10 +36,15 @@ function App() {
 
         <Route path="/dashboard" element={<StudentDashboard />} />
         <Route path="/course" element={<CourseDashboard />} />
+        <Route path="/assignments" element={<AllAssignment />} />
+        <Route path="/calendar" element={<Calendar />} />
         <Route path="/assignment" element={<AssignmentDashboard />} />
         <Route path="/teacher" element={<TeacherView />} />
         <Route path="/class" element={<ClassPage />} />
-        
+        <Route path="/grades" element={<AllGrades />} />
+        <Route path="/account" element={<Account />} />
+
+
       </Routes>
     </div>
   );
